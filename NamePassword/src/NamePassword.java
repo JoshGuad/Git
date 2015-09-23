2c9296c72bc31b9390ddbc808bb0040a568e342d
JoshGuad <jguad.sv@gmail.com>
2015-09-23 14:39:24 -0400

    Initial

---
+++ NamePassword/src/NamePassword.java
@@ -0,0 +1,34 @@
+
+     
+
+	import java.util.Scanner;
+	public class NamePassword
+	{
+	  
+		  public static void main(String[] args)
+		  {
+	    
+	    System.out.println("Frist enter your username");
+	    System.out.println("Second enter your password.");
+	    
+	    
+	    
+	    
+	    Scanner keyboard = new Scanner(System.in);
+	  
+	  // keyboard.nextLine();
+	   String username = keyboard.next();
+	   String password = keyboard.next();
+	   
+	    keyboard.close();
+	    
+	    
+	    
+	    
+	    System.out.println(username);
+	    System.out.println("XXXXX");
+	    
+	  
+	  }
+	  
+	  }
